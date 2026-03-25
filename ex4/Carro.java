b5021f3e8761d769fdecd03687a06e7b6fdbcede
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-25 17:33:47 -0300

    Create Carro.java

---
+++ ex4/Carro.java
@@ -0,0 +1,16 @@
+package main;
+public class Carro {
+    private double km;
+    private double preco;
+    
+    public Carro(){
+        km=200;
+        preco= 15000;
+    }
+    public double getKm(){
+        return this.km;
+    }
+    public double getPreco(){
+        return this.preco;
+    }
+}
