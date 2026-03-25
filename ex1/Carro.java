d9ed0ee08bb2a865d1f132d6847df79a53bacb89
Thalyson_Gama_gc <thalysoncosta567@gmail.com>
2026-03-25 15:00:01 -0300

    Add Carro class with methods to open trunk and turn on radio

---
+++ ex1/Carro.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Carro extends Transporte{
+    public void  abrirPortaMalas(){ 
+        System.out.println("Porta malas aberto...");
+    }
+    public void ligarRadio(){
+        System.out.println("Radio ligada...");
+    }
+    
+}
